283c7214e88be75072000b7f11da442ee6ede23a
fikile kentane <fikilek@innopen.co.za>
2020-01-11 12:31:52 +0200

    main menu working proper, still to do page menu and main body

---
+++ static/scripts/ireps.js
@@ -0,0 +1,11 @@
+console.log('START running ireps.js')
+
+$(document).ready(function() {
+  console.log('START ireps.js document ready function')
+
+
+
+  console.log('END ireps.js document ready function')
+});
+
+console.log('END running ireps.js')
